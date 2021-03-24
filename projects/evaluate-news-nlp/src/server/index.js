13dc7ab63edd34319ea963ae5e9288eed8ae35fa
marksav85 <mark.saville@outlook.com>
2021-03-24 12:50:36 +0100

    Add bodyparser and cors to server file

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,21 +1,30 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-const dotenv = require('dotenv');
+var bodyParser = require('body-parser')
+var cors = require('cors')
+/*const dotenv = require('dotenv');
 dotenv.config();
 
 var textapi = new meaning_cloud({
   application_key: api_key.env.API_KEY
-});
+});*/
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
