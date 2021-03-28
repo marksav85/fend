4ca945f43e8b9cd38d6ec6073686abf83c24b36c
marksav85 <mark.saville@outlook.com>
2021-03-28 18:50:32 +0200

    fix: Change optimiser plugin to minimizer

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -2,14 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-//const WorkboxPlugin = require('workbox-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     optimization: {
-      minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      minimizer: [new TerserPlugin({}), new CssMinimizerWebpackPlugin({})]
     },
     output: {
         libraryTarget: 'var',
@@ -34,7 +34,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        //new WorkboxPlugin.GenerateSW(),
+        new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
