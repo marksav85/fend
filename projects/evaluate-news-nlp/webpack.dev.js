1918665c7e71010c234ed772b51f201495c67c56
marksav85 <mark.saville@outlook.com>
2021-03-29 13:36:52 +0200

    fix: Remove clean webpack plugin for output.clean option

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
-        library: 'Client'
+        library: 'Client',
+        clean: true, // Clean the output directory before emit.
     },
     mode: 'development',
     devtool: 'source-map',
@@ -29,15 +29,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
         })
     ]
 }
