bb14fa16eaa5ad50e5fb85b7100d64bc2bde95eb
marksav85 <mark.saville@outlook.com>
2021-04-03 12:17:03 +0200

    fix: Update post route to work correctly

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -6,12 +6,34 @@ function handleSubmit(event) {
 
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8090/test')
+
+console.log("::: Form Submitted :::")
+
+const postData = async ( url = '', data = {})=>{
+    const req = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+    try {
+      const newData = await req.json();
+      console.log(newData);
+      return newData
+    }catch(error) {
+      console.log("Error", error);
+    }
+};
+
+fetch('http://localhost:8090/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
     })
 }
+
+postData('http://localhost:8090/data', {url: formText})
 
 export { handleSubmit }
